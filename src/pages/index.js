792fa6888863368602c870a0f9721f4d444488ac
Bryan Irace <bryan@irace.me>
2019-02-18 13:10:01 -0500

    Add some initial data

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,43 @@ import Layout from "../components/layout";
 import { Link } from "gatsby";
 import React from "react";
 import SEO from "../components/seo";
+import { graphql } from "gatsby";
 
-const IndexPage = () => (
+export const query = graphql`
+  query HomePageQuery {
+    allJobsYaml {
+      edges {
+        node {
+          username
+          jobs {
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to Matchmaking.</p>
-    <p>Now go build something great.</p>
+    <h1>Matchmaking</h1>
+    {data.allJobsYaml.edges.map(edge => {
+      return (
+        <div>
+          <div>@{edge.node.username}</div>
+          {edge.node.jobs.map(job => {
+            return (
+              <>
+                <div>{job.title}</div>
+                <div>{job.description}</div>
+              </>
+            );
+          })}
+        </div>
+      );
+    })}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
