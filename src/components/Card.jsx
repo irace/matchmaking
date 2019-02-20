26f2f1c017c5985050bf677dda87ef6ca1b08025
Bryan Irace <bryan@irace.me>
2019-02-20 14:47:53 -0500

    Add Markdown styling to job titles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 import styled from "styled-components";
 
 const Article = styled.article.attrs({ className: "mw5 bg-white br3 pa2 pa3-ns mv3 ba b--black-10 mr3" })``;
+
+const Title = styled.h1.attrs({ className: "f4 normal" })``;
 
 const Card = props => {
   return (
     <Article>
       <div className="tc">
-        <h1 className="f4">{props.title}</h1>
+        <Title>
+          <ReactMarkdown source={props.title} />
+        </Title>
         <hr className="mw3 bb bw1 b--black-10" />
       </div>
       <p className="lh-copy measure center f6 black-70">{props.description}</p>
